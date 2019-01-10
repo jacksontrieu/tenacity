8c138351364535840a0452e72e5b5ef6e024004d
jacksontrieu <jackson@twied.com>
2019-01-10 22:59:43 +1100

    Update devise sign_in action to return the JWT token in the response body.

--- tenacity_api/app/controllers/user/sessions_controller.rb
+++ tenacity_api/app/controllers/user/sessions_controller.rb
@@ -1,30 +1,22 @@
-# frozen_string_literal: true
-
 class User::SessionsController < Devise::SessionsController
   respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # Custom response so that we include the JSON Web Token in the API
+    # response. This is required by the 'ember-simple-auth-token' library we
+    # use, ideally we should extract the token from the 'Authentication'
+    # response header instead.
+    render json: {
+      id: resource.id,
+      email: resource.email,
+      token: current_token
+    }
+  end
 
   private
 
@@ -34,5 +26,9 @@ class User::SessionsController < Devise::SessionsController
 
   def respond_to_on_destroy
     head :no_content
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
