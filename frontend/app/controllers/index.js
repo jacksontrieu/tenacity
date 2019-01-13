8be8c6403dab5914f71cbe3c4eb81598fccc0fe8
jacksontrieu <jackson@twied.com>
2019-01-13 20:19:11 +1100

    Removed obsolete actions & properties from ember index controller.

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,25 +1,4 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  showLogin: false,
-  showSignUp: false,
-  signUpErrorMessage: '',
-
-  actions: {
-    showChoices() {
-      this.set('showLogin', false);
-      this.set('showSignUp', false);
-    },
-    showLogin() {
-      this.set('showLogin', true);
-      this.set('showSignUp', false);
-    },
-    showSignUp() {
-      this.set('showLogin', false);
-      this.set('showSignUp', true);
-    },
-    showSignUpError(message) {
-      this.set('signUpErrorMessage', message);
-    }
-  }
 });
