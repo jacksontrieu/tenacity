c2b2b7e5a75e7cbd06ee6a98c016fb514be45697
jacksontrieu <jackson@twied.com>
2019-01-15 00:16:55 +1100

    Fix typo in mirage fake users response data.

--- frontend/tests/utils/responses/users.js
+++ frontend/tests/utils/responses/users.js
@@ -23,7 +23,7 @@ export const getUsersResponse = {
       phone: '0400 123 456'
     },
     {
-      id: 2,
+      id: 3,
       email: 'another@user.com',
       first_name: 'Another',
       last_name: 'User',
